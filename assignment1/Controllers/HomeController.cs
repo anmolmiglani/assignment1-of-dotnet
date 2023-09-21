e133f3fa2e957ffc08ea4bd6e6ca04d70e3d3b24
Anmol Miglani <200534220@student.georgianc.on.ca>
2023-09-21 07:17:25 +0530

    first commit

---
+++ assignment1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using assignment1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Assigment01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Brief()
+        {
+            return View();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
